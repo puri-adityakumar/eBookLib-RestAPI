3d8ad9b6fc3d31a57e44d6ff53b48e65fd286170
puri-adityakumar <adityakumar90.ak@gmail.com>
2025-02-02 21:42:52 +0530

    created userModel

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,6 +1,7 @@
 
 import { Request, Response, NextFunction } from 'express';
 import createHttpError from 'http-errors';
+import userModel from './userModel';
 
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +12,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
           const error = createHttpError(400, 'All fields are required');
           return next(error);
      }
+
+     // database call
+     const user = await  userModel.findOne({email});
+     if (user) {
+          const error = createHttpError(400, 'User already exists');
+          return next(error);
+     }
+     
      res.json({ message: "User registered" });
 }
 
